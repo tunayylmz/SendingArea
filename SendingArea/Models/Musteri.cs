1ca6a345e6d916729ae4a772e577a8e6a2f61f2d
Tunayylmz <tunaysabriyilmaz@gmail.com>
2020-04-06 00:07:54 +0300

    ty

--- SendingArea/Models/Musteri.cs
+++ SendingArea/Models/Musteri.cs
@@ -1,27 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
 
 namespace SendingArea.Models
 {
-    public class CourierCompany
+    public class Musteri
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Phone { get; set; }
-        public string Address { get; set; }
-        public string TaxNo { get; set; }
-        public string TaxOffice { get; set; }
+        public string Ad_Soyad { get; set; }
+        public int Cep_Tel { get; set; }
+        public int TC_No { get; set; }
+        public string E_Posta { get; set; }
+        public string sifre { get; set; }
 
-        public bool WriteClass()
+        public bool MusteriKaydiOlusturma()
         {
             string conString = ConfigurationManager.ConnectionStrings["Baglanti"].ConnectionString;
             SqlConnection baglanti = new SqlConnection(conString);
             baglanti.Open();
-            string q = "insert into CourierCompany(Name,Phone,Address,TaxNo, TaxOffice)values('"+Name+"',"+ Phone+",'"+Address+"',"+TaxNo+",'"+TaxOffice+"')";
+            string q = "insert into Musteri_Kayit(Ad_Soyad,Cep_Tel,TC_No,E_Posta,sifre)values('" + Ad_Soyad + "'," + Cep_Tel + "," + TC_No + ",'" + E_Posta + "','" + sifre + "')";
             SqlCommand com = new SqlCommand(q, baglanti);
             com.ExecuteNonQuery();
             return false;
